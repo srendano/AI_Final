31dc2eda001b9538b54559a3d247025a7b85fe97
Spencer Rendano <srendano@umass.edu>
2019-04-24 21:46:30 +0200

    Order_Cnt test

--- student/gameProblem.py
+++ student/gameProblem.py
@@ -33,14 +33,6 @@ class GameProblem(SearchProblem):
         #  If actions are in the list of movement actions
         # (North, East, etc.), they will generate moves in the map when the path is
         # followed, but you can use additional actions
-        
-        #Series of If statements? Like if you can move West, then add that to list of actions
-
-        #I assume this is imposed by a constraint (i.e. you can / can't move this way because of this)
-
-        #USE getAttribute(<position>,<key>) to check vals in attribute section of map
-        #and decide type of terrain, num or orders
-
 
         # self.CONFIG['map_size'][0] - 1 -> x-coordinate (max) east-most
         # self.CONFIG['map_size'][1] - 1 -> y-coordinate (max) south-most
@@ -81,11 +73,12 @@ class GameProblem(SearchProblem):
 
 
         #Load
-        if state == self.POSITIONS['pizza']:
+        if state == self.POSITIONS['pizza'][0] and self.PIZZA_CNT < 2:
             actions.append('Load')
 
         #Unload
-        if state == self.POSITIONS['customer1'][0] or state ==self.POSITIONS['customer1'][1] or ['customer2']:
+        #if state == self.POSITIONS['customer1'][0] or state == self.POSITIONS['customer1'][1] or state == self.POSITIONS['customer2']:
+        if self.getAttribute(state, 'unload') and self.PIZZA_CNT > 0:
             actions.append('Unload')
             #Check if building needs pizza (Get pending Requests)
             #Prob use"unload": True,
@@ -117,6 +110,19 @@ class GameProblem(SearchProblem):
         elif action == 'South':
             next_state = (state[0], state[1] + 1)
 
+        elif action == 'Load':
+            next_state = state
+            self.PIZZA_CNT += 1
+
+        elif action == 'unload':
+            next_state = state
+            self.PIZZA_CNT -= 1
+            tileAttributes = self.MAP[position[0]][position[1]][2]
+            tileAttributes['objects'] -= 1
+            if tileAttributes['objects'] == 0:
+                 tileAttributes['unload'] = false
+                 self.ORDER_CNT -= 1
+
         else:
             next_state = state #default val
 
@@ -138,7 +144,7 @@ class GameProblem(SearchProblem):
         '''Returns true if state is the final state
         '''
         #Compare state to self.goal
-        return state == self.GOAL; 
+        return (state == self.GOAL and self.ORDER_CNT == 0)
 
         #MAYBE return to base after?
 
@@ -169,7 +175,18 @@ class GameProblem(SearchProblem):
 
 
         initial_state = self.AGENT_START
-        final_state= (9,3) 
+        final_state = self.POSITIONS['pizza'][0]
+        pizza_cnt = 0
+        order_cnt = 0
+        for x in range (self.CONFIG['map_size'][0] ):
+            for y in range (self.CONFIG['map_size'][1] ):
+                curr_state = (x,y)
+                order_num = self.getPendingRequests(curr_state)
+                if order_num == None:
+                    order_num = 0
+                order_cnt += order_num
+                print(str(x) + ' ,' + str(y) + 'order count: ' + str(order_cnt))
+
 
         #Tuple if state is location NOT list or dict
         
@@ -180,12 +197,12 @@ class GameProblem(SearchProblem):
         #algorithm= simpleai.search.depth_first
         #algorithm= simpleai.search.limited_depth_first
 
-        return initial_state,final_state,algorithm
+        return initial_state,final_state, pizza_cnt, order_cnt, algorithm
         
     def printState (self,state):
         '''Return a string to pretty-print the state '''
         
-        pps=''
+        pps= 'Pizza Count ' + str(self.PIZZA_CNT)
         return (pps)
 
     def getPendingRequests (self,state):
@@ -193,7 +210,13 @@ class GameProblem(SearchProblem):
             MUST return None if the position is not a customer.
             This information is used to show the proper customer image.
         '''
-        return None
+        #if state == self.MAP['customer1'][0] or state == self.MAP['customer1'][1] or state == self.MAP['customer2']:
+        if self.getAttribute(state, 'unload'):
+            tileAttributes = self.MAP[state[0]][state[1]][2]
+            if 'objects' in tileAttributes.keys():
+                return tileAttributes['objects']
+        else:
+            return None
 
     # -------------------------------------------------------------- #
     # --------------- DO NOT EDIT BELOW THIS LINE  ----------------- #
@@ -228,13 +251,15 @@ class GameProblem(SearchProblem):
         self.CONFIG=conf
         self.AGENT_START = tuple(conf['agent']['start'])
 
-        initial_state,final_state,algorithm = self.setup()
+        initial_state,final_state,pizza_cnt, order_cnt, algorithm = self.setup()
         if initial_state == False:
             print ('-- INITIALIZATION FAILED')
             return True
       
         self.INITIAL_STATE=initial_state
         self.GOAL=final_state
+        self.PIZZA_CNT = pizza_cnt
+        self.ORDER_CNT = order_cnt
         self.ALGORITHM=algorithm
         super(GameProblem,self).__init__(self.INITIAL_STATE)
             
@@ -242,4 +267,3 @@ class GameProblem(SearchProblem):
         return True
         
     # END initializeProblem 
-
